5467abea789f2dbe9dd77e7fb620a0744e729756
Harshvardhan2212 <harshvardhan.z2002@gmail.com>
2026-03-05 02:24:51 +0530

    correcting a wrong api endpoints

--- src/Redux/Slices/AuthApis.js
+++ src/Redux/Slices/AuthApis.js
@@ -1,86 +1,86 @@
 import { api } from "../api";
 let token = localStorage.getItem("token")
 export const authApi = api.injectEndpoints({
-    endpoints: (builder) => ({
-        getUserData: builder.query({
-            query: (auth) => ({
-                url: "my-profile",
-                method: "GET",
-                headers: { Authorization: `Bearer ${auth}` }
-            }),
-            providesTags: ["use", "multishop", "UserProfile"],
-            // keepUnusedDataFor: 1
-        }),
-        addUserProfile: builder.mutation({
-            query: (data) => ({
-                url: "update-profile",
-                method: "POST",
-                body: data,
-                headers: { Authorization: `Bearer ${token}` }
-            }),
-            invalidatesTags: ["use"]
+  endpoints: (builder) => ({
+    getUserData: builder.query({
+      query: (auth) => ({
+        url: "my-profile",
+        method: "GET",
+        headers: { Authorization: `Bearer ${auth}` }
+      }),
+      providesTags: ["use", "multishop", "UserProfile"],
+      // keepUnusedDataFor: 1
+    }),
+    addUserProfile: builder.mutation({
+      query: (data) => ({
+        url: "update-profile",
+        method: "POST",
+        body: data,
+        headers: { Authorization: `Bearer ${token}` }
+      }),
+      invalidatesTags: ["use"]
 
-        }),
-        postUserSignInData: builder.mutation({
-            query: (values) => ({
-                url: "user-login",
-                method: "POST",
-                body: values,
-            }),
-            invalidatesTags : ["UserProfile"]
-        }),
-        
-        postUserSignUpData: builder.mutation({
-            query: (data) => ({
-                url: "user-register",
-                method: "POST",
-                body: data,
-            }),
+    }),
+    postUserSignInData: builder.mutation({
+      query: (values) => ({
+        url: "user-login",
+        method: "POST",
+        body: values,
+      }),
+      invalidatesTags: ["UserProfile"]
+    }),
 
-        }),
-        postChangePasswordData: builder.mutation({
-            query: (data) => ({
-                url: "change-password",
-                method: "POST",
-                body: data,
-                headers: { Authorization: `Bearer ${token}` }
-            }),
-        }),
-        postForgetPasswordData: builder.mutation({
-            query: (data) => ({
-                url: "password/email",
-                method: "POST",
-                body: data,
-            }),
-        }),
-        postResetPasswordData: builder.mutation({
-            query: (data) => ({
-                url: "reset-password",
-                method: "POST",
-                body: data,
-            }),
-        }),
-        postLogoutData: builder.mutation({
-            query: (payload) => ({
-                url: "user-logout",
-                method: "POST",
-                headers: { Authorization: `Bearer ${payload?.token}` }
-            }),
-            invalidatesTags: ["logout"]
-        }),
+    postUserSignUpData: builder.mutation({
+      query: (data) => ({
+        url: "user-register",
+        method: "POST",
+        body: data,
+      }),
 
-    })
+    }),
+    postChangePasswordData: builder.mutation({
+      query: (data) => ({
+        url: "change-password",
+        method: "POST",
+        body: data,
+        headers: { Authorization: `Bearer ${token}` }
+      }),
+    }),
+    postForgetPasswordData: builder.mutation({
+      query: (data) => ({
+        url: "forgot-password",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    postResetPasswordData: builder.mutation({
+      query: (data) => ({
+        url: "reset-password",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    postLogoutData: builder.mutation({
+      query: (payload) => ({
+        url: "user-logout",
+        method: "POST",
+        headers: { Authorization: `Bearer ${payload?.token}` }
+      }),
+      invalidatesTags: ["logout"]
+    }),
+
+  })
 })
 
 
 export const {
-    useAddUserProfileMutation,
-    useGetUserDataQuery,
-    usePostUserSignInDataMutation,
-    usePostUserSignUpDataMutation,
-    usePostChangePasswordDataMutation,
-    usePostForgetPasswordDataMutation,
-    usePostResetPasswordDataMutation,
-    usePostLogoutDataMutation,
-    useLazyGetUserDataQuery
+  useAddUserProfileMutation,
+  useGetUserDataQuery,
+  usePostUserSignInDataMutation,
+  usePostUserSignUpDataMutation,
+  usePostChangePasswordDataMutation,
+  usePostForgetPasswordDataMutation,
+  usePostResetPasswordDataMutation,
+  usePostLogoutDataMutation,
+  useLazyGetUserDataQuery
 } = authApi
