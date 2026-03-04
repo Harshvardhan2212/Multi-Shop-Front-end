c6dfe7c11bf73dfc9878bb5c2670104c63bfb93f
Harshvardhan2212 <harshvardhan.z2002@gmail.com>
2026-03-04 18:50:32 +0530

    current state

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const api = createApi({
-    reducerPath: "Apis",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://192.168.1.204:8088/api/" }),
-    tagTypes: ["multishop", "UserProfile", "wish"],
+  reducerPath: "Apis",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://streamglitch.space/api/" }),
+  tagTypes: ["multishop", "UserProfile", "wish"],
 
-    endpoints: (bulider) => ({})
+  endpoints: (bulider) => ({})
 })
 
 
