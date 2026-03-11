b2bc06fac548b2b1775a3c3ac60ec4d7cb657b11
Harshvardhan2212 <harshvardhan.z2002@gmail.com>
2026-03-11 20:37:37 +0530

    remove env and moving api endpoint to process from env

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
+const baseUrl = process.env.REACT_APP_BASE_URL
+
 export const api = createApi({
   reducerPath: "Apis",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost4:8010/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   tagTypes: ["multishop", "UserProfile", "wish"],
 
   endpoints: (bulider) => ({})
